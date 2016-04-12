ede1ecabfdd4fc3eb851f9940303a8eed1ad4c10
paour <github@paour.com>
2016-04-12 09:46:58 +0200

    Communication Javascript - Natif
    Using ugly hack of a js-native interface

--- app/src/main/java/com/lelivrescolaire/testtechnique/activity/PageActivity.kt
+++ app/src/main/java/com/lelivrescolaire/testtechnique/activity/PageActivity.kt
@@ -1,7 +1,9 @@
 package com.lelivrescolaire.testtechnique.activity
 
+import android.net.Uri
 import android.os.AsyncTask
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.util.Xml
 import android.webkit.WebView
@@ -31,7 +33,7 @@ class PageActivity : AppCompatActivity() {
 
         setContentView(webView)
 
-        object : AsyncTask<Void,Void,String>() {
+        object : AsyncTask<Void, Void, String>() {
             override fun doInBackground(vararg params: Void?): String {
                 val content = StringBuilder()
                 resources.assets.open("page.html").bufferedReader().forEachLine {
@@ -47,6 +49,21 @@ class PageActivity : AppCompatActivity() {
                 webView.setWebViewClient(object : WebViewClient() {
                     override fun onPageFinished(view: WebView?, url: String?) {
                         webView.evaluateJavascript("displayLineNumbers()", null);
+                    }
+
+                    override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                        val uri = Uri.parse(url)
+
+                        if (uri.scheme == "native") {
+                            AlertDialog.Builder(this@PageActivity)
+                                    .setTitle(R.string.from_javascript)
+                                    .setMessage(uri.host)
+                                    .setPositiveButton(android.R.string.ok, null)
+                                    .show()
+                            return true
+                        } else {
+                            return false
+                        }
                     }
                 })
             }
